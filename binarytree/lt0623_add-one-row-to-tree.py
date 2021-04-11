35d3cba46cd765eef10ea18293b26818cd5b1ab5
BaoshuBradyqi <qjjmail@126.com>
2021-04-10 21:55:51 -0400

    'update'

---
+++ binarytree/lt0623_add-one-row-to-tree.py
@@ -0,0 +1,30 @@
+"""
+recursive
+if depth is more than 2, it would become add to left/right node with depth - 1
+corner cases would be depth == 1, in which root would become left child
+and when depth == 2,  would add to both left and right child
+
+time complexity would be O(depth)
+space complexity would be O(1)
+
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def addOneRow(self, root: TreeNode, val: int, depth: int) -> TreeNode:
+        if depth == 1:
+            return TreeNode(val, root)
+        elif depth == 2:
+            root.left  = TreeNode(val,  root.left)
+            root.right = TreeNode(val, right=root.right)
+            return root
+        else:
+            if root.left is not None:
+                root.left = self.addOneRow(root.left, val, depth - 1)
+            if root.right is not None:
+                root.right = self.addOneRow(root.right, val, depth - 1)
+            return root
