d1132e2ee6b568af49cf8f30d538dd523aa8e330
BaoshuBradyqi <qjjmail@126.com>
2021-04-26 21:24:41 -0400

    'threading'

---
+++ threading/lt1114_print-in-order.py
@@ -0,0 +1,25 @@
+"""
+using event to schedule the funs
+"""
+from threading import Event
+class Foo:
+    def __init__(self):
+        self.e1 = Event()
+        self.e2 = Event()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.e1.set()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.e1.wait()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self.e2.set()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.e2.wait()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
